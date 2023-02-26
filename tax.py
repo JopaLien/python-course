824979c3d59b4122f484164b06bdaba33d2f890f
Nikolai <nikdenof@gmail.com>
2023-02-26 19:54:42 +0300

    Reviewed your code, please look at it

--- tax.py
+++ tax.py
@@ -1,5 +1,7 @@
 import time
-a=int(input())
+# Должен быть энтер между импортом библиотеки
+a=int(input()) # a = int(input())
 for i in range(a):
     print("штраф 1000 рублей")
     time.sleep(0.516)
+# Остальное топ
