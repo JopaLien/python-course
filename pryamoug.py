1f4657396afe7471c3430b3edbe52af08cdbba2e
Nikolai <nikdenof@gmail.com>
2023-03-24 22:27:21 +0300

    Changed pifagor

--- pryamoug.py
+++ pryamoug.py
@@ -1,14 +1,31 @@
-def pifagor():
-    if c == 0 and a != 0 and b != 0:
-        print("гипотенуза равна",(a**2+b**2)**0.5)
-    if c != 0 and (b == 0 and a == 0):
-        print("не хватает данных")
-    elif a == 0 and c != 0 or b == 0 and c != 0:
-        print("нужный вам катет равен", (c**2-a**2)**0.5 if b == 0  else (c**2-b**2)**0.5)
-    elif a != 0 and b != 0 and c != 0:
-        print("у вас уже всё есть")
+def pifagor(a=None, b=None, c=None):
+    if not c:
+        print("определение гипотенузы:")
+        return gipotenus_find(a, b)
+    elif not (a and b):
+        print("finding missing catet")
+        return catet_find(c, a, b)
     else:
-        print("не хватает данных")
+        print("not enough data or everything is defined")
+
+def gipotenus_find(a=None, b=None):
+    if a and b:
+        c = (int(a)**2+int(b)**2)**0.5
+        return c
+    else:
+        print("not enough data")
+
+def catet_find(c, a=None, b=None):
+    if a:
+        print("Determening catet b")
+        return calculate_catet(a, c)
+    else:
+        print("Determening catet a")
+        return calculate_catet(b, c) 
+
+def calculate_catet(known_catet, c):
+    unknown_catet = (int(c) ** 2 - int(known_catet) ** 2) ** 0.5
+    return unknown_catet
 
 def pinus():
     if a != 0 and c != 0:
@@ -22,15 +39,15 @@ def copis():
         print("не хватает данных")
 
 print("введите значение гипотенузы(если её нет напишите 0)")
-c = int(input())
+c = input()
 print("введите катет противолежащий(если нет введите 0 )")
-a = int(input())
+a = input()
 print("введите катет прилежащий(если нет введите 0 )")
-b = int(input())
+b = input()
 print("какая операция вам нужна?","(1)найти неизвестную сторону","(2)найти синус","(3)найти косинус", sep="\n")
 n = int(input())
 if n == 1:
-    pifagor()
+    print(pifagor(a, b, c))
 if n == 2:
     pinus()
 if n == 3:
